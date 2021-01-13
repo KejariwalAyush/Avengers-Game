8ec70e2bbc4ab95d300c48adc75fc4b87d29487b
Ayush Kejariwal <kejayush29@gmail.com>
2021-01-13 14:20:05 +0530

    added multiple heros and villans

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ robo = Power('Iron-Robot', 35, 1000, 'bad')
 charge = Power('Charge HP', 15, 500, 'good')
 drink = Power('Boost HP', 5, 150, 'good')
 
-ironman_powers = [thunder, ironfist, robo, charge, drink]
-# Add loki powers as well
+avenger_powers = [thunder, ironfist, robo, charge, drink]
+# Add tvillan powers as well
 
 # Tools for persons
 hammer = Tool('Hammer', 'bad', 'Damages 900 HP of enemy.', 900)
@@ -29,7 +29,7 @@ sheild = Tool('Sheild', 'both',
 sheildpro = Tool('SheildPro', 'both',
                  'Nullify next Villan attack & damage them with 1500 HP', 1500)
 
-ironman_tools = [
+avenger_tools = [
     {'tool': hammer, 'qty': 10},
     {'tool': granade, 'qty': 5},
     {'tool': arrow, 'qty': 15},
@@ -41,16 +41,16 @@ ironman_tools = [
 ]
 
 # Initiate Persons
-ironman = Person('Iron-Man', 4000, 150, 350, 50, ironman_powers, ironman_tools)
-# antman = Person('Ant-Man\t', 3000, 100, 250, 40, [], [])
-# thor = Person('Thor\t', 3500, 120, 300, 30, [], [])
+ironman = Person('Iron-Man', 4000, 150, 350, 50, avenger_powers, avenger_tools)
+antman = Person('Ant-Man\t', 3000, 100, 250, 40, avenger_powers, avenger_tools)
+thor = Person('Thor\t', 3500, 120, 300, 30, avenger_powers, avenger_tools)
 
-# thanos = Person('Thanos\t', 10000, 500, 500, 30, [], [])
-loki = Person('Loki\t', 6000, 240, 500, 10, [], [])
+thanos = Person('Thanos\t', 20000, 500, 500, 30, [], [])
+loki = Person('Loki\t', 15000, 240, 500, 10, [], [])
 
 
-# avengers = [ironman, antman, thor]
-# villans = [thanos, loki]
+avengers = [ironman, antman, thor]
+villans = [thanos, loki]
 
 running = True
 i = 0
@@ -59,104 +59,136 @@ print("A Villan ATTACKS!")
 
 while running:
     print("-----------------------------------------")
-    # print("\n")
+    print("\n")
     print("NAME                 HP                                     Coins")
-    ironman.get_stats()
-    # print("\n")
-    # print("NAME                                   HP                        ")
-    loki.get_enemy_stats()
+    for avenger in avengers:
+        avenger.get_stats()
+    print("\n")
+    print("NAME                                   HP                        ")
+    for villan in villans:
+        villan.get_enemy_stats()
 
-    # Avenger Attacks
-    ironman.choose_action()
-    choice = int(input('\tChoose Action: ')) - 1
+    # Check if battle is over
 
-    # Use of simple attack
-    if choice == 0:
-        damage = ironman.generate_damage()
-        loki.take_damage(damage)
-        print('You Attacked '+loki.name.replace('\t', '') +
-              ' for', damage, 'points of damage.')
+    # Check if Player won
+    if len(villans) == 0:
+        print("You win!")
+        running = False
+        break
 
-        if loki.get_hp() == 0:
-            print(loki.name.replace('\t', '') + ' has Died.'+'\n\nYou Win!')
-            running = False
-            continue
-
-    # for use of powers
-    elif choice == 1:
-        ironman.choose_power()
-        power_choice = int(input('\tChoose Power: '))-1
-
-        if power_choice == -1:
-            continue
-
-        power = ironman.powers[power_choice]
-        pdamage = power.generate_damage()
-
-        if power.cost > ironman.get_coins():
-            print('\nNot Enough Coins.\n')
-            continue
-
-        ironman.reduce_coins(power.cost)
-
-        if power.type == 'good':
-            ironman.take_heal(pdamage)
-            print('You used '+power.name+' for',
-                  pdamage, 'points for healing yourself')
-            continue
-        elif power.type == 'bad':
-            loki.take_damage(pdamage)
-            print('You used '+power.name+' for', pdamage, 'points of damage')
-
-        if loki.get_hp() == 0:
-            print(loki.name.replace('\t', '') + ' has Died.'+'\n\nYou Win!')
-            running = False
-            continue
-
-    # for use of tools
-    elif choice == 2:
-        ironman.choose_tools()
-        tool_choice = int(input('\tChoose Tool: ')) - 1
-
-        if tool_choice == -1:
-            continue
-
-        tool = ironman.tools[tool_choice]['tool']
-        ironman.tools[tool_choice]['qty'] -= 1
-
-        if tool.type == 'bad':
-            loki.take_damage(tool.prop)
-            print('You used ' + tool.name+' for',
-                  tool.prop, 'points of damage')
-        elif tool.type == 'good':
-            ironman.take_heal(tool.prop)
-            print('You used ' + tool.name+' for',
-                  tool.prop, 'points for healing yourself')
-            if tool.name == 'MediCare':
-                continue
-        else:
-            loki.take_damage(tool.prop)
-            print('You used '+tool.name+' for' + tool.desc)
-            if loki.get_hp() == 0:
-                print(loki.name.replace('\t', '') +
-                      ' has Died.'+'\n\nYou Win!')
-                running = False
-            continue
-
-        if loki.get_hp() == 0:
-            print(loki.name.replace('\t', '') + ' has Died.'+'\n\nYou Win!')
-            running = False
-            continue
+    # Check if Enemy won
+    elif len(avengers) == 0:
+        print("Your Villans have defeated you!")
+        running = False
+        break
 
     # else:
+    #     print('Tied')
     #     running = False
+    #     break
 
+    # Avenger Attacks
+    for avenger in avengers:
+        if len(villans) == 0 or len(avengers) == 0:
+            break
+        avenger.choose_action()
+        choice = int(input("    Choose action: ")) - 1
+
+        tvillan = villans[random.randrange(0, len(villans))]
+
+        # Use of simple attack
+        if choice == 0:
+            damage = avenger.generate_damage()
+            tvillan.take_damage(damage)
+            print('You Attacked '+tvillan.name.replace('\t', '') +
+                  ' for', damage, 'points of damage.')
+
+            if tvillan.get_hp() == 0:
+                print(tvillan.name.replace('\t', '') +
+                      ' has Died.'+'\n\nYou Win!')
+                villans.remove(tvillan)
+                continue
+
+        # for use of powers
+        elif choice == 1:
+            avenger.choose_power()
+            power_choice = int(input('\tChoose Power: '))-1
+
+            if power_choice == -1:
+                continue
+
+            power = avenger.powers[power_choice]
+            pdamage = power.generate_damage()
+
+            if power.cost > avenger.get_coins():
+                print('\nNot Enough Coins.\n')
+                continue
+
+            avenger.reduce_coins(power.cost)
+
+            if power.type == 'good':
+                avenger.take_heal(pdamage)
+                print('You used '+power.name+' for',
+                      pdamage, 'points for healing yourself')
+                continue
+            elif power.type == 'bad':
+                tvillan.take_damage(pdamage)
+                print('You used '+power.name+' for',
+                      pdamage, 'points of damage')
+
+            if tvillan.get_hp() == 0:
+                print(tvillan.name.replace('\t', '') +
+                      ' has Died.'+'\n\nYou Win!')
+                villans.remove(tvillan)
+                continue
+
+        # for use of tools
+        elif choice == 2:
+            avenger.choose_tools()
+            tool_choice = int(input('\tChoose Tool: ')) - 1
+
+            if tool_choice == -1:
+                continue
+
+            tool = avenger.tools[tool_choice]['tool']
+            if avenger.tools[tool_choice]['qty'] <= 0:
+                continue
+            avenger.tools[tool_choice]['qty'] -= 1
+
+            if tool.type == 'bad':
+                tvillan.take_damage(tool.prop)
+                print('You used ' + tool.name+' for',
+                      tool.prop, 'points of damage')
+            elif tool.type == 'good':
+                avenger.take_heal(tool.prop)
+                print('You used ' + tool.name+' for',
+                      tool.prop, 'points for healing yourself')
+                if tool.name == 'MediCare':
+                    continue
+            else:
+                tvillan.take_damage(tool.prop)
+                print('You used '+tool.name+' for' + tool.desc)
+                if tvillan.get_hp() == 0:
+                    print(tvillan.name.replace('\t', '') +
+                          ' has Died.'+'\n\nYou Win!')
+                    villans.remove(tvillan)
+                continue
+
+            if tvillan.get_hp() == 0:
+                print(tvillan.name.replace('\t', '') +
+                      ' has Died.'+'\n\nYou Win!')
+                villans.remove(tvillan)
+                continue
+
+    if len(avengers) == 0 or len(villans) == 0:
+        continue
     # Villan attacks back
-    vdamage = loki.generate_damage()
-    ironman.take_damage(vdamage)
-    print(loki.name.replace('\t', '') +
+    tvillan = villans[random.randrange(0, len(villans))]
+    vdamage = tvillan.generate_damage()
+    tavenger = avengers[random.randrange(0, len(avengers))]
+    tavenger.take_damage(vdamage)
+    print(tvillan.name.replace('\t', '') +
           ' attacked you for', vdamage, 'points of damage.')
-    if ironman.get_hp() == 0:
-        print(ironman.name.replace('\t', '') +
-              ' has been defeated by the Villans.\n\nYou Lose!')
-        running = False
+    if tavenger.get_hp() == 0:
+        print(tavenger.name+' has been defeated by the Villans.\n\nYou Lose!')
+        avengers.remove(tavenger)
